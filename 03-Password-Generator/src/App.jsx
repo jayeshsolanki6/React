1d0a77b766e6995466d5126fbfe58452549eb973
jayeshsolanki6 <mejayeshsolanki@gmail.com>
2026-05-04 21:21:48 +0530

    2

--- 03-Password-Generator/src/App.jsx
+++ 03-Password-Generator/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
 function PasswordGenerator() {
   const [passwordLen, setPasswordLen] = useState(8);
   const [numberRequired, setNumberRequired] = useState(false);
   const [symbolRequired, setSymbolRequired] = useState(false);
   const [password, setPassword] = useState('');
+  const passwordRef = useState(null);
 
   const generatePassword = useCallback(() => {
     let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -16,29 +17,58 @@ function PasswordGenerator() {
       newPassword += charset[Math.floor(Math.random() * charset.length)];
     }
     setPassword(newPassword);
-  }, [passwordLen, numberRequired, symbolRequired]); // dependencies
+  }, [passwordLen, numberRequired, symbolRequired]);
+
+  const copyPasswordToClipboard = () => {
+    navigator.clipboard.writeText(password);
+    passwordRef.current.select();
+  };
+
+  useEffect(()=>{
+    generatePassword()
+  }, [passwordLen, numberRequired, symbolRequired])
 
   return (
-    <div>
-      <p>Password: {password}</p>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={generatePassword}>
-        Generate
-      </button>
+    <div className='h-screen w-full flex flex-col justify-center items-center'>
+      <div className='border-none rounded-full overflow-auto bg-blue-100 pl-3 w-1/3'>
+        <input 
+        className='outline-none bg-transparent'
+        ref={passwordRef}
+        type="text" 
+        placeholder='Password'
+        value={password}
+        readOnly
+        />
+        <button
+        onClick={copyPasswordToClipboard}
+        className='px-3 py-1 bg-blue-600 rounded-full text-white font-bold'
+        >copy</button>
+      </div>
+      
+      <div className='flex justify-center gap-0.5'>
+        <input 
+          type="range" 
+          min="8" max="50"
+          id='len'
+          value={passwordLen}
+          onChange={(e) => setPasswordLen(e.target.value)}
+        />
+        <label htmlFor="len">Length : {passwordLen}</label>
+        
+        <input type="checkbox"
+          id='num'
+          checked={numberRequired}
+          onChange={(e) => setNumberRequired(prev => !prev)}
+        />
+        <label htmlFor="num">Number</label>
 
-      <input type="range" min="8" max="50"
-        value={passwordLen}
-        onChange={(e) => setPasswordLen(Number(e.target.value))}
-      />
-
-      <input type="checkbox"
-        checked={numberRequired}
-        onChange={(e) => setNumberRequired(e.target.checked)}
-      />
-
-      <input type="checkbox"
-        checked={symbolRequired}
-        onChange={(e) => setSymbolRequired(e.target.checked)}
-      />
+        <input type="checkbox"
+          id='sym'
+          checked={symbolRequired}
+          onChange={(e) => setSymbolRequired(prev => !prev)}
+        />
+        <label htmlFor="sym">Symbol</label>
+      </div>
     </div>
   );
 }
